06ae80a4254d4c356678af49f49ca673dcb30961
AdriellySoares <113360657+AdriellySoares@users.noreply.github.com>
2022-10-13 13:03:20 -0300

    atulizar

---
+++ index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './index.css';
+
+export default function BannerFooter() {
+    return (
+        <React.Fragment>
+        <section>
+<div className="r2">
+    <div className="r21">
+        <div className="r23">
+            <div className="r24">
+                <div className="r25">
+                <div className="r26">
+                    <div className="r2">
+                    Oferta especial
+                    </div>
+                    <div className="r27">
+                    <span>Air Jordan edição de colecionador</span>
+                    </div>
+                    <div className="r271">
+                        <span>Lorem ipsum dolo sit amet, consectetur adipiscing elit, sed do<br>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</br> <br>ad minim veniam, quis nostrud exercitation laboris nisi ut aliqui</br></span>
+                    </div>
+                    <div className="r273">
+                    <button>Ver Oferta</button>
+                    </div>
+                </div>
+            </div>
+            <div className="imagem">
+                <div className="imagem2">
+                    <img src="tenis.png" alt="meal"></img>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+    </section>
+    </React.Fragment>
+)
+}
