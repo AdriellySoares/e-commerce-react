06257438e85f1f637c48fbda577f12b77a31d09f
AdriellySoares <113360657+AdriellySoares@users.noreply.github.com>
2022-10-13 13:09:38 -0300

    add arquivo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ export default function BannerFooter() {
                     <span>Air Jordan edição de colecionador</span>
                     </div>
                     <div className="r271">
-                        <span>Lorem ipsum dolo sit amet, consectetur adipiscing elit, sed do<br>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</br> <br>ad minim veniam, quis nostrud exercitation laboris nisi ut aliqui</br></span>
+                        <span>Lorem ipsum dolo sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation laboris nisi ut aliqui</span>
                     </div>
                     <div className="r273">
                     <button>Ver Oferta</button>
